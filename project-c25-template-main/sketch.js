f433858f618f005d62ad650dcccb7cc23b84d70a
Ananya141207 <75618420+Ananya141207@users.noreply.github.com>
2021-05-21 12:36:22 +0530

    Add files via upload

---
+++ project-c25-template-main/sketch.js
@@ -0,0 +1,51 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj,groundObject	,Paper;
+var world;
+
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	groundObject=new ground(width/2,670,width,20);
+	dustbinObj=new dustbin(1200,650);
+	Paper = new paper (100,600,10);
+
+	leftSide=new dustbin(550,620,20,100);
+   bottomSide=new dustbin(610,660,100,20);
+   rightSide=new dustbin(670,620,100);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(230);
+ Engine.update(engine);
+
+  groundObject.display();
+  dustbinObj.display();
+  Paper.display();
+  keyPressed();
+  drawSprites();
+
+}
+function keyPressed(){
+if (keyCode ===Up_ARROW){
+
+	Matter.Body.applyForce(Paper.body,Paper.body.position,{x:15,y:-15})
+}
+
+
+}
